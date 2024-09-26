ca88627a6facc3b816d312dfce8e37e63605fd15
lakshmimungara <lakshmimungara2997@gmail.com>
2024-09-26 19:42:15 +0530

    expense project

--- variables.tf
+++ variables.tf
@@ -6,6 +6,10 @@ variable "environment"{
     default = "dev"
 }
 
+variable "vpc_cidr"{
+    default = "10.0.0.0/16"
+}
+
 variable "common_tags"{
     default = {
         Project = "expense"
@@ -21,7 +25,6 @@ variable "public_subnet_cidrs"{
 variable "private_subnet_cidrs"{
     default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
-
 variable "database_subnet_cidrs"{
     default = ["10.0.21.0/24", "10.0.22.0/24"]
 }
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,9 @@
 module "vpc"{
-    source = "../terraform-aws-vpc"
+    # source = "../terraform-aws-vpc"
+
+    # By using this, it pulls the module from git directly no need to be in workspace
+    #after completion of module, that we will keep in git and use it
+    source = "git::https://github.com/lakshmimungara/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     environment = var.environment
     common_tags = var.common_tags
